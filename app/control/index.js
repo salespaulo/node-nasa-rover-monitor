12f428723394ccf147222030daa8df18096cf5b3
paulosalesmobly <paulo.sales@mobly.com.br>
2022-03-04 18:48:55 -0300

    Refactory code to use command functions and tests success

--- app/control/index.js
+++ app/control/index.js
@@ -2,7 +2,11 @@
  * Rover control management.
  */
 
+const { MarsPlateau } = require("../mars");
 const { Rover } = require("../rover");
+
+const { turn, move } = require("./command");
+
 /**
  * Rover commands is:
  *   - "L": Turn 90 degrees on left.
@@ -14,15 +18,33 @@ const COMMANDS = ["L", "R", "M"];
 
 class RoverControl {
   /**
-   *
-   * @param {Rover} rover
+   * Rover that landed on a plateau in planet Mars.
+   * @param {Rover} rover Rover orientation and position.
+   * @param {MarsPlateau} plateau Mars plateau where rover was landed.
    */
-  constructor(rover) {
+  constructor(rover, plateau) {
     this.rover = rover;
+    this.plateau = plateau;
   }
 
   execute(command) {
-    return null;
+    const rover = this.rover;
+    const plateau = this.plateau;
+    const validCommand = COMMANDS.indexOf(command) >= 0 ? command : "I";
+
+    if (validCommand === "I") {
+      return this.rover;
+    }
+
+    if (validCommand === "R") {
+      return turn(rover);
+    }
+
+    if (validCommand === "L") {
+      return turn(rover, true);
+    }
+
+    return move(rover, plateau);
   }
 }
 
--- app/control/index.test.js
+++ app/control/index.test.js
@@ -1,7 +1,71 @@
+/**
+ * NASA tests to the control of rovers.
+ */
+
+const { Mars } = require("../mars");
 const { Rover } = require("../rover");
 
-test("Rover control instance of default rover", () => {
-  const defaultRover = new Rover();
+const { RoverControl } = require("./index");
 
-  expect(defaultRover.control).not.toBeNull();
+test("Rover control instance", () => {
+  const mars = new Mars();
+  const rover = new Rover();
+  const plateau = mars.getPlateau();
+
+  const roverControl = new RoverControl(rover, plateau);
+  expect(roverControl.rover).not.toBeNull();
+  expect(roverControl.plateau).not.toBeNull();
+});
+
+test("Rover control execute left command", () => {
+  const mars = new Mars();
+  const rover = new Rover();
+  const plateau = mars.getPlateau();
+
+  const roverControl = new RoverControl(rover, plateau);
+
+  const newRover = roverControl.execute("L");
+
+  expect(newRover.x).toEqual(rover.x);
+  expect(newRover.y).toEqual(rover.y);
+  expect(newRover.orientation).toEqual("W");
+});
+
+test("Rover control execute right command", () => {
+  const mars = new Mars();
+  const plateau = mars.getPlateau();
+  const rover = new Rover();
+
+  const roverControl = new RoverControl(rover, plateau);
+  const newRover = roverControl.execute("R");
+
+  expect(newRover.x).toEqual(rover.x);
+  expect(newRover.y).toEqual(rover.y);
+  expect(newRover.orientation).toEqual("E");
+});
+
+test("Rover control execute move command", () => {
+  const mars = new Mars();
+  const plateau = mars.getPlateau();
+  const rover = new Rover();
+
+  const roverControl = new RoverControl(rover, plateau);
+  const newRover = roverControl.execute("M");
+
+  expect(newRover.x).toEqual(rover.x);
+  expect(newRover.y).toEqual(rover.y + 1);
+  expect(newRover.orientation).toEqual("N");
+});
+
+test("Rover control execute invalid command", () => {
+  const mars = new Mars();
+  const plateau = mars.getPlateau();
+  const rover = new Rover();
+
+  const roverControl = new RoverControl(rover, plateau);
+  const newRover = roverControl.execute("Invalid Command");
+
+  expect(newRover.x).toEqual(rover.x);
+  expect(newRover.y).toEqual(rover.y);
+  expect(newRover.orientation).toEqual(rover.orientation);
 });
