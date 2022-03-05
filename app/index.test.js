74c2850d019e06bb4375ed9714a929dc9937f872
paulosalesmobly <paulo.sales@mobly.com.br>
2022-03-04 22:18:42 -0300

    Implements tests to rover application on fail

--- app/index.test.js
+++ app/index.test.js
@@ -2,25 +2,45 @@
  * NASA rover application tests.
  */
 
-const { RoverApplication, RoverAppInvalidCommandsError } = require("./index");
+const {
+  RoverApplication,
+  RoverAppInvalidLengthCommandsError,
+  RoverAppInvalidCommandsError,
+} = require("./index");
 
-test("Rover app instance", () => {
-  const roverApp = new RoverApplication();
-  expect(roverApp.roverMonitor).not.toBeNull();
-});
-
-test("Rover app start with letters null", () => {
+test("Rover app start with invalid null letters", () => {
   const roverApp = new RoverApplication();
 
   expect(() => roverApp.start(null)).toThrow(
-    new RoverAppInvalidCommandsError(0)
+    new RoverAppInvalidLengthCommandsError(0)
   );
 });
 
-test("Rover app start with letters only, 5 5 ENTER", () => {
+test("Rover app start with invalid length letters only 2 commands", () => {
   const roverApp = new RoverApplication();
 
-  expect(() => roverApp.start("5 5\n")).toThrow(
-    new RoverAppInvalidCommandsError(1)
-  );
+  expect(() => roverApp.start("5 5\n1 2 N")).toThrow("Invalid Length");
+});
+
+test("Rover app start with invalid letters commands", () => {
+  const roverApp = new RoverApplication();
+  const letters = "5 5\n0 0 N\nXXMM\n";
+
+  expect(() => roverApp.start(letters)).toThrow("Invalid Syntax");
+});
+
+test("Rover app start with correct letters commands", () => {
+  const roverApp = new RoverApplication();
+  const letters = "5 5\n0 0 N\nRMM\n";
+  const output = roverApp.start(letters);
+
+  expect(output).toEqual("2 0 E");
+});
+
+test("Rover app start with - FoxBit Test", () => {
+  const roverApp = new RoverApplication();
+  const letters = "5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM";
+  const output = roverApp.start(letters);
+
+  expect(output).toEqual("1 3 N\n5 1 E");
 });
